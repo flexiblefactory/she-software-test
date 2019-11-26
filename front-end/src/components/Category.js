b92cbfd7a639783e6cc549ccdb01449b83a3fece
flexiblefactory <hal@Hals-MacBook-Pro.local>
2019-11-26 22:16:16 +0000

    Render a hierarchy of categories recursively

---
+++ front-end/src/components/Category.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { observer } from "mobx-react"
+import { makeStyles } from '@material-ui/core/styles';
+import TreeItem from '@material-ui/lab/TreeItem';
+
+const useStyles = makeStyles({ category: { color: 'hotpink', width: 400 }});
+  
+const Cat = ({ cat, cats, onSelect }) => {
+
+  const classes = useStyles();
+
+  const childCats = cats.all.filter(c => {
+    return c.parent === cat.id
+  })
+
+  const children = childCats.map(c => (
+    <Cat onSelect={onSelect} cats={cats} key={'cat' + c.id} cat={c}></Cat>
+  ))
+
+  return (
+    <TreeItem
+      className={classes.category}
+      onClick={() => onSelect(cat)}
+      key={cat.id}
+      nodeId={cat.id ? 'cat' + cat.id.toString() : 'root'}
+      label={cat.label}>
+      {children.length === 0 ? null : children}
+    </TreeItem>
+  );
+}
+
+export default observer(Cat)
